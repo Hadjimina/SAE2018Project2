23c05559ee48bb31994c4dad14a4e6475e053940
valentinkunze <valentin_kunze@hotmail.de>
2018-05-29 16:55:12 +0100

    updated test files

--- src/Test_ok_const_cornercases.java
+++ src/Test_ok_const_cornercases.java
@@ -1,11 +1,7 @@
 public class Test_ok_const_cornercases {
 	public static Car m1(int j) {
 		Car car = new Car();
-<<<<<<< HEAD
 		car.setSpeed(-2);	
-=======
-		car.setSpeed(-2);
->>>>>>> 88fef6d1faa12269b708398ff823e0620d151d25
 		return car;
 	}
 }
